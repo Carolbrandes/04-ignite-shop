ed5a776219a2f5c974e0129ecfe69a70d01e5828
carol <carolina.brandes@assurant.com>
2024-08-27 14:41:35 -0400

    Use Link from next/link for navigate to product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { HomeContainer, Product } from "../styles/pages/home";
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from "keen-slider/react";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
 
@@ -29,13 +30,15 @@ export default function Home({ products }: HomeProps) {
 
       {
         products.map(product => (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         ))
       }
 
@@ -61,9 +64,9 @@ export const getStaticProps: GetStaticProps = async () => {
       imageUrl: product.images[0],
       price: price?.unit_amount
         ? new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          }).format(price.unit_amount / 100)
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100)
         : 'Preço não disponível', // Fallback if unit_amount is null or undefined
     };
   });
