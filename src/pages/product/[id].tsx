38b8693f7ace1741c67425184dd9b97e944a1ab4
carol <carolbrandes23@gmail.com>
2024-08-30 17:13:10 -0400

    get data from stripe on product's page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,29 +1,86 @@
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
-import { useParams } from 'next/navigation';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Stripe from 'stripe';
+import { stripe } from '../../lib/stripe';
 
-export default function Product() {
-    const params = useParams();
-
-    // Check if params exists and has an id
-    if (!params || !params.id) {
-        return <div>Loading...</div>;
+interface ProductProps {
+    product: {
+        id: string
+        name: string
+        imageUrl: string
+        price: string
+        description: string
     }
+}
 
-    const { id } = params;
-    console.log("🚀 ~ Product ~ id:", id);
+export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if (isFallback) return <p>Loading...</p>
 
     return (
         <ProductContainer>
-            <ImageContainer></ImageContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+            </ImageContainer>
 
             <ProductDetails>
-                <h1>Camiseta x</h1>
-                <span>R$ 79,90</span>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos praesentium blanditiis rem ratione dolorum consectetur eos voluptates error assumenda doloribus aspernatur voluptatum dignissimos at cumque qui, autem recusandae, fugit sequi?</p>
+                <p>{product.description}</p>
 
                 <button>Comprar agora</button>
             </ProductDetails>
         </ProductContainer>
     );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    // pode carregar nos paths os 5 produtos mais vendidos, mais acessados
+    return {
+        paths: [
+            { params: { id: 'prod_QdHKr7obozdrx0' } }
+        ],
+        fallback: true
+    }
+}
+
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params?.id
+
+    let product = {} as any
+
+    if (productId) {
+
+        product = await stripe.products.retrieve(productId, {
+            expand: ['default_price']
+        })
+
+
+        const price = product.default_price as Stripe.Price;
+
+        product = {
+            id: product.id,
+            name: product.name,
+            imageUrl: product.images[0],
+            price: price?.unit_amount
+                ? new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(price.unit_amount / 100)
+                : 'Preço não disponível', // Fallback if unit_amount is null or undefined
+            description: product.description
+        };
+
+    }
+
+
+    return {
+        props: { product },
+        revalidate: 60 * 60 * 1 // 1 hour
+    }
 }
